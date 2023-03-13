36bc995a4d44d5b50394f49502b5739a3e3f655f
Ruslan Maksimkin <127615611+ruslanmaksimkin@users.noreply.github.com>
2023-03-13 22:54:49 +0300

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -16,6 +16,6 @@ public class Converter {
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
         float dollar = Converter.rubleToDollar(140);
-        System.out.println("140 bubles are " + dollar + " dollar.");
+        System.out.println("140 rubles are " + dollar + " dollar.");
     }
 }
