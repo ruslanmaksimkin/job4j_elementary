eb7ae3eeaa211e1c60ab65dea576bd2e8d594116
Ruslan Maksimkin <127615611+ruslanmaksimkin@users.noreply.github.com>
2023-03-14 14:21:59 +0300

    7. Расстояние между точками в системе координат [#188]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,28 +1,28 @@
 package ru.job4j.condition;
 
 public class Point {
-    public static double distanse1(int x1, int x2) {
-        double rsl = x2 - x1;
-        return rsl;
+    public static double distance1(int x1, int y1, int x2, int y2) {
+        double rsl1 = x2 - x1;
+        return rsl1;
     }
 
-    public static double distanse2(int y1, int y2) {
-        double rsl = y2 - y1;
-        return rsl;
+    public static double distance2(int x1, int y1, int x2, int y2) {
+        double rsl2 = y2 - y1;
+        return rsl2;
+    }
+
+    public static double distance3(int x1, int y1, int x2, int y2) {
+        double rsl3 = Math.pow(x2 - x1, 2);
+        return rsl3;
+    }
+
+    public static double distance4(int x1, int y1, int x2, int y2) {
+        double rsl4 = Math.pow(y2 - y1, 2);
+        return rsl4;
     }
 
     public static void main(String[] args) {
-        double result1 = Point.distanse1(0, 2);
-        System.out.println(result1);
-        double result2 = Point.distanse1(0, 0);
-        System.out.println(result2);
-        double result3 = Math.pow(result1, 2);
-        System.out.println(result3);
-        double result4 = Math.pow(result2, 2);
-        System.out.println(result4);
-        double result5 = result3 + result4;
+        double result5 = Math.sqrt(distance3(0, 0, 2, 0) + distance4(0, 0, 2, 0));
         System.out.println(result5);
-        double result6 = Math.sqrt(result5);
-        System.out.println(result6);
     }
 }
