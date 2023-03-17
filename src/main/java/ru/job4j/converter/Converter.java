19d3d238024b52f5727214070ad6574f610c8d05
Ruslan Maksimkin <127615611+ruslanmaksimkin@users.noreply.github.com>
2023-03-17 12:47:45 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -1,32 +1,31 @@
-
 package ru.job4j.converter;
 
-    public class Converter {
+public class Converter {
 
-        public static float rubleToEuro(float value) {
-            float rsl = value / 70;
-            return rsl;
-        }
-
-        public static float rubleToDollar(float value) {
-            float rsl = value / 60;
-            return rsl;
-        }
-
-        public static void main(String[] args) {
-            float euro = Converter.rubleToEuro(140);
-            System.out.println("140 rubles are " + euro + " euro.");
-            float in = 140;
-            float expected = 2;
-            float out = Converter.rubleToEuro(in);
-            boolean passed = expected == out;
-            System.out.println("140 rubles are 2. Test result : " + passed);
-            float dollar = Converter.rubleToDollar(140);
-            System.out.println("140 rubles are " + dollar + " dollar.");
-            float in1 = 140;
-            float axpected = 2.3333333F;
-            float out1 = Converter.rubleToDollar(in1);
-            boolean passed10 = expected == out1;
-            System.out.println("140 rubles are 2.3333333. Test result : " + passed1);
-        }
+    public static float rubleToEuro(float value) {
+        float rsl = value / 70;
+        return rsl;
     }
+
+    public static float rubleToDollar(float value) {
+        float rsl = value / 60;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result: " + passed);
+        float dollar = Converter.rubleToDollar(140);
+        System.out.println("140 bubles are " + dollar + " dollar.");
+        float in1 = 140;
+        float expected1 = 2.3333333F;
+        float out1 = Converter.rubleToDollar(in1);
+        float passed1 = expected1 = out;
+        System.out.println("140 rubles are 2.3333333. Test result " + passed);
+    }
+}
