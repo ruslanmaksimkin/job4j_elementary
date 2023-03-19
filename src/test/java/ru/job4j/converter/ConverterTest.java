dab2f4c1559bab28bb5953b2d117eb5c9a8c974a
Ruslan Maksimkin <127615611+ruslanmaksimkin@users.noreply.github.com>
2023-03-19 10:48:45 +0300

    1. Модульные тесты. [#159435]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -8,7 +8,7 @@ public class ConverterTest {
     @Test
     public void whenConvert200RblThen2dot3333Dlr() {
         float in = 3850;
-        float expected = 50.6578947F;
+        float expected = 50.6578F;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0001F;
         Assert.assertEquals(expected, out, eps);
