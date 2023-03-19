951375d609a608438f54bcd320785e4d91b01e40
Ruslan Maksimkin <127615611+ruslanmaksimkin@users.noreply.github.com>
2023-03-19 11:50:09 +0300

    1.1. Что такое import [#504773]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -14,8 +14,14 @@ public class MathCalculator {
                 + division(first, second);
     }
 
+    public static double sumForAll(double first, double second) {
+         return sum(first, second) + multiply(first, second)
+                 + subtraction(first, second) + division(first, second);
+    }
+
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
         System.out.println("Результат расчета равен: " + subAndDivision(55, 10));
+        System.out.println("Результат расчета равен: " + sumForAll(15, 15));
     }
 }
