b13faf5019870765171814e490e2ad5b0baf6974
Ruslan Maksimkin <127615611+ruslanmaksimkin@users.noreply.github.com>
2023-03-20 10:55:43 +0300

    1. Модульные тесты. [#159435]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -3,8 +3,7 @@ package ru.job4j.converter;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        float rsl = value / 70;
-        return rsl;
+        return value / 70;
     }
 
     public static float rubleToDollar(float value) {
