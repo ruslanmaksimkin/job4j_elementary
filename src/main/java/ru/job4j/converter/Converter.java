42e2412aacc964ecce658e6bf595247d7babb205
Ruslan Maksimkin <127615611+ruslanmaksimkin@users.noreply.github.com>
2023-03-20 12:03:13 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -7,7 +7,7 @@ public class Converter {
     }
 
     public static float rubleToDollar(float value) {
-        return value / 65;
+        return value / 650;
     }
 
     public static void main(String[] args) {
