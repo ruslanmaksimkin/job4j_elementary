3bb1641b2d5b06fa31a30aef570612d3bad8edde
Ruslan Maksimkin <127615611+ruslanmaksimkin@users.noreply.github.com>
2023-03-25 15:34:41 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -1,17 +1,19 @@
 package ru.job4j.loop;
 
 public class Counter {
-    public static int sum(int start, int finish) {
+    public static int sumByEven(int start, int finish) {
         int sum = 0;
-        for (int index = start; index <= finish; index++) {
-            sum = sum + index;
+        for (int x = start; x <= finish; x++) {
+            if (x % 2 == 0) {
+                sum = x;
+            }
         }
         return sum;
     }
 
     public static void main(String[] args) {
-        System.out.println(sum(0, 10));
-        System.out.println(sum(3, 8));
-        System.out.println(sum(1, 1));
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
     }
 }
