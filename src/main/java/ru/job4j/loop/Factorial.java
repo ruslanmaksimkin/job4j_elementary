5f3fde377a5db0ef7792fa927f11eac7511cc3be
Ruslan Maksimkin <127615611+ruslanmaksimkin@users.noreply.github.com>
2023-03-26 11:32:09 +0300

    5.2. Создать программу, вычисляющую факториал. [#193]

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class Factorial {
     public static int calc(int n) {
         int result = 1;
-        for (int x = 1; x <= n; x++) {
+        for (int x = 2; x <= n; x++) {
             result = result * x;
         }
         return result;
