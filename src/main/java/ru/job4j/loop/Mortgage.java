1ec032a57e49a07541f5f0465786802105c7d05f
Ruslan Maksimkin <127615611+ruslanmaksimkin@users.noreply.github.com>
2023-03-28 19:15:41 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, double salary, double percent) {
+        int year = 0;
+        while (amount > 0) {
+            amount = amount + amount * percent / 100;
+            amount = amount - salary;
+            year++;
+        }
+        return year;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(year(100, 70, 50));
+    }
+}
