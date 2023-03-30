680f709b72831bb53214320cce63a4ca2d3a5d47
Ruslan Maksimkin <127615611+ruslanmaksimkin@users.noreply.github.com>
2023-03-30 12:32:02 +0300

    5.9. Крест в псевдографике.  [#177182]

---
+++ src/main/java/ru/job4j/loop/Slash.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+public class Slash {
+    public static void draw(int size) {
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                if (cell == row) {
+                    System.out.print("X");
+                } else if (row + cell == size - 1) {
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw by 3");
+        draw(3);
+        System.out.println("Draw by 5");
+        draw(5);
+    }
+}
